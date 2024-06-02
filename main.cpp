71842b8ab962a983bad2b98aa0d9287f44380111
petermdtran246 <petermdtran246@gmail.com>
2024-06-02 15:27:30 +0700

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void Display()
+{
+    int A[]={2,4,6,8,10};
+    for(auto x: A)
+    {
+        cout << x << " ";
+    }
+}
+
+int main()
+{
+    Display();
+    return 0;
+
+}
